385c1fa34096aa5fcd319416733d51f21561a94a
ShleinaKseniya <159529942+ShleinaKseniya@users.noreply.github.com>
2024-05-23 16:47:20 +0300

    Add files via upload

---
+++ Шлеина_7_2уровень.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+
+public struct Jumper
+{
+    private string _name;
+    private int _lengthrate;
+    private int _rate;
+    private int _score;
+
+    public Jumper(string name, int length, int rate1, int rate2, int rate3, int rate4, int rate5)
+    {
+        _name = name;
+        _lengthrate = (length - 120) * 2 + 60;
+        int[] rate = new int[5];
+        rate[0] = rate1;
+        rate[1] = rate2;
+        rate[2] = rate3;
+        rate[3] = rate4;
+        rate[4] = rate5;
+        for (int i = 0; i < rate.Length - 1; i++)
+        {
+            for (int j = i; j < rate.Length; j++)
+            {
+                if (rate[i] < rate[j])
+                {
+                    int temp = rate[i];
+                    rate[i] = rate[j];
+                    rate[j] = temp;
+                }
+            }
+        }
+        _rate = rate[1] + rate[2] + rate[3];
+        _score = _lengthrate + _rate; // финальные очки спортсмена
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public int GetScore()
+    {
+        return _score;
+    }
+}
+
+abstract class SkiJumping
+{
+    protected string _discipline;
+    protected string _name;
+    protected int _score;
+    public SkiJumping(Jumper jumper)
+    {
+        _discipline = "";
+        _name = jumper.GetName();
+        _score = jumper.GetScore();
+    }
+
+    public virtual int GetPoints()
+    {
+        return _score;
+    }
+
+    public virtual void Write()
+    {
+        Console.WriteLine("{0}\t   {1}", _name, _score);
+    }
+
+    public virtual void WriteDis()
+    {
+        Console.WriteLine(_discipline);
+    }
+}
+
+class J120m : SkiJumping
+{
+    public J120m(Jumper jumper) : base(jumper) { _discipline = "120 метров"; _name = jumper.GetName(); _score = jumper.GetScore(); }
+
+    public override void Write()
+    {
+        base.Write();
+    }
+
+    public override void WriteDis()
+    {
+        base.WriteDis();
+    }
+
+    public override int GetPoints()
+    {
+        return base.GetPoints();
+    }
+
+}
+
+class J180m : SkiJumping
+{
+    public J180m(Jumper jumper) : base(jumper) { _discipline = "180 метров"; _name = jumper.GetName(); _score = jumper.GetScore(); }
+
+    public override void Write()
+    {
+        base.Write();
+    }
+
+    public override void WriteDis()
+    {
+        base.WriteDis();
+    }
+
+    public override int GetPoints()
+    {
+        return base.GetPoints();
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        Jumper[] jumper = new Jumper[10];
+
+        // 120 метров
+        jumper[0] = new Jumper("Иванов", 115, 15, 17, 11, 10, 12);
+        jumper[1] = new Jumper("Лебедев", 120, 11, 15, 12, 14, 13);
+        jumper[2] = new Jumper("Попов", 129, 20, 19, 17, 18, 16);
+        jumper[3] = new Jumper("Смирнов", 125, 17, 17, 16, 18, 19);
+        jumper[4] = new Jumper("Козлов", 128, 20, 18, 18, 15, 14);
+
+        // 180 метров
+
+        jumper[5] = new Jumper("Кузнецов", 175, 15, 17, 11, 10, 12);
+        jumper[6] = new Jumper("Крылов", 178, 11, 15, 12, 14, 13);
+        jumper[7] = new Jumper("Соколов", 179, 20, 19, 17, 18, 16);
+        jumper[8] = new Jumper("Пушкин", 176, 17, 17, 16, 18, 19);
+        jumper[9] = new Jumper("Молотов", 163, 20, 18, 18, 15, 14);
+
+        J120m[] j120 = new J120m[5];
+        J180m[] j180 = new J180m[5];
+
+        for (int i = 0; i < 5; i++)
+        {
+            j120[i] = new J120m(jumper[i]);
+        }
+
+        for (int i = 5; i < 10; i++)
+        {
+            j180[i - 5] = new J180m(jumper[i]);
+        }
+
+        Sort(j120);
+        Sort(j180);
+        Print(j120);
+        Print(j180);
+
+        static void Sort(SkiJumping[] jumper)
+        {
+            for (int i = 0; i < jumper.Length - 1; i++)
+            {
+                for (int j = i; j < jumper.Length; j++)
+                {
+                    if (jumper[i].GetPoints() < jumper[j].GetPoints())
+                    {
+                        SkiJumping temp = jumper[i];
+                        jumper[i] = jumper[j];
+                        jumper[j] = temp;
+                    }
+                }
+            }
+        }
+
+        static void Print(SkiJumping[] jumper)
+        {
+            jumper[0].WriteDis();
+            foreach (SkiJumping jump in jumper)
+            {
+                jump.Write();
+            }
+        }
+
+    }
+}
---
+++ Шлеина_7_3уровень.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+class FootballTeam
+{
+    private string name;
+    private int Scored;
+    private int Failed;
+    private int points;
+
+    public FootballTeam(string name)
+    {
+        this.name = name;
+        Scored = 0;
+        Failed = 0;
+        points = 0;
+    }
+
+    public string Name { get { return name; } set { name = value; } }
+
+    public void Result(int scored, int conceded)
+    {
+        Scored += scored;
+        Failed += conceded;
+        if (scored > conceded) points += 3;
+        else if (scored == conceded) points += 1;
+    }
+
+    public int Points { get { return points; } }
+    public int Difference { get { return Scored - Failed; } }
+
+    public static void Print(FootballTeam[] teams)
+    {
+        Console.WriteLine(" Place   Team   Gender   Points");
+        for (int i = 0; i < teams.Length; i++)
+        {
+            Console.WriteLine("{0,2} {1,10} {2,12} {3,3}", i + 1, teams[i].Name, teams[i] is MaleFootballTeam ? "Male Team" : "Female Team", teams[i].Points);
+        }
+    }
+}
+
+class MaleFootballTeam : FootballTeam
+{
+    public MaleFootballTeam(string name) : base(name)
+    {
+    }
+}
+
+class FemaleFootballTeam : FootballTeam
+{
+    public FemaleFootballTeam(string name) : base(name)
+    {
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        FootballTeam[] maleTeams = new FootballTeam[]
+ {
+    new MaleFootballTeam("A"),
+    new MaleFootballTeam("B"),
+    new MaleFootballTeam("C")
+ };
+
+        FootballTeam[] femaleTeams = new FootballTeam[]
+        {
+    new FemaleFootballTeam("D"),
+    new FemaleFootballTeam("E"),
+    new FemaleFootballTeam("F")
+        };
+        PlayMatches(maleTeams);
+        PlayMatches(femaleTeams);
+        FootballTeam bestMaleTeam = GetBestTeam(maleTeams);
+        FootballTeam bestFemaleTeam = GetBestTeam(femaleTeams);
+
+        FootballTeam overallBestTeam = (bestMaleTeam.Points > bestFemaleTeam.Points || (bestMaleTeam.Points == bestFemaleTeam.Points && bestMaleTeam.Difference > bestFemaleTeam.Difference)) ? bestMaleTeam : bestFemaleTeam;
+
+        Console.WriteLine("Лучшая команда: {0}", overallBestTeam.Name);
+    }
+
+    static void PlayMatches(FootballTeam[] teams)
+    {
+        for (int i = 0; i < teams.Length; i++)
+        {
+            for (int j = i + 1; j < teams.Length; j++)
+            {
+                if ((teams[i] is MaleFootballTeam && teams[j] is MaleFootballTeam) ||
+                    (teams[i] is FemaleFootballTeam && teams[j] is FemaleFootballTeam))
+                {
+                    Match(ref teams[i], ref teams[j]);
+                }
+            }
+        }
+    }
+
+    static void Match(ref FootballTeam team1, ref FootballTeam team2)
+    {
+        Random random = new Random();
+        int scored = random.Next(0, 5);
+        int conceded = random.Next(0, 5);
+        team1.Result(scored, conceded);
+        team2.Result(conceded, scored);
+    }
+
+    static FootballTeam GetBestTeam(FootballTeam[] teams)
+    {
+        FootballTeam bestTeam = teams[0];
+        for (int i = 1; i < teams.Length; i++)
+        {
+            if (teams[i].Points > bestTeam.Points ||
+                (teams[i].Points == bestTeam.Points && teams[i].Difference > bestTeam.Difference))
+            {
+                bestTeam = teams[i];
+            }
+        }
+        return bestTeam;
+    }
+}
